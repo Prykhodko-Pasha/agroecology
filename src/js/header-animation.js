2a7804e6ad4b612991c6a81c445d3741cd5364ce
Prykhodko-Pasha <prykhodko.p@gmail.com>
2021-07-06 18:25:44 +0300

    added header animation

--- src/js/header-animation.js
+++ src/js/header-animation.js
@@ -3,12 +3,24 @@ const refs = {
     logo: document.querySelector(".logo"),
     menu: document.querySelector(".menu"),
 };
-console.log(refs.header);
+// console.dir(refs.header);
+
+
 
 const animateHeader = () => {
+    let top = window.scrollY;
+    // console.log(top);
+    
+    if(top > 1) {
     refs.header.classList.add('header-is-scrolled');
     refs.logo.classList.add('logo-is-scrolled');
-    refs.menu.classList.add('menu-is-scrolled');
+        refs.menu.classList.add('menu-is-scrolled');
+    }
+    else {
+        refs.header.classList.remove('header-is-scrolled');
+        refs.logo.classList.remove('logo-is-scrolled');
+        refs.menu.classList.remove('menu-is-scrolled');
+    }
 }
 
 window.addEventListener("scroll", animateHeader);
