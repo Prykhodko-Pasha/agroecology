b6cfedbbc31aef909b9591d1509699a827763663
Prykhodko-Pasha <prykhodko.p@gmail.com>
2021-07-20 18:14:57 +0300

    added mob-menu

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,8 +1,10 @@
 const refs = {
   burgerBtn: document.querySelector(".burger"),
   burgerIcon: document.querySelector(".burger__icon"),
+  mobMenu: document.querySelector(".mob-menu"),
 };
 
 refs.burgerBtn.addEventListener("click", () => {
   refs.burgerBtn.classList.toggle("is-open");
+  refs.mobMenu.classList.toggle("is-open");
 });
