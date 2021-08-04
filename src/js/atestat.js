8320357aa6671795136e22aaed3e8fac11317e80
Prykhodko-Pasha <prykhodko.p@gmail.com>
2021-08-04 11:29:45 +0300

    added lightbox

--- src/js/atestat.js
+++ src/js/atestat.js
@@ -1,28 +1,52 @@
 const refs = {
   atestat: document.querySelector(".atestat"),
-  // body: document.querySelector("body"),
-  // menu: document.querySelector(".menu"),
+  lightbox: document.querySelector(".lightbox"),
+  lightbox__image: document.querySelector(".lightbox__image"),
+  overlay: document.querySelector(".lightbox__overlay"),
+  closeBtn: document.querySelector('[data-action="close-lightbox"]'),
 };
-// console.dir(refs.atestat);
-// let scrollPrev = 0;
+refs.atestat.addEventListener("click", onOpenModal);
 
-// const onImgClose = (e) => {
-//   console.log(e.currentTarget);
-//   console.log(e.target);
-//   if (e.currentTarget !== e.target) {
-//     refs.atestat.classList.remove("atestat-clicked");
-//     // refs.body.removeEventListener("click", onImgClose);
-//   }
-// };
-
-// const listenBody = () => {
-//   refs.body.addEventListener("click", onImgClose);
-// };
-
-const onImgClick = (e) => {
-  refs.atestat.classList.toggle("atestat-clicked");
+function onOpenModal(e) {
+  // e.preventDefault();
+  if (!e.target.classList.contains("lightbox__min-image")) {
+    return;
+  }
   // console.log(e.target);
-  // listenBody();
-};
+  refs.lightbox.classList.add("is-open");
+  refs.lightbox__image.src = e.target.dataset.source;
+  refs.lightbox__image.alt = e.target.alt;
 
-refs.atestat.addEventListener("click", onImgClick);
+  // IMG_IDX = imgArr.indexOf(e.target);
+  // console.log(IMG_IDX);
+  window.addEventListener("keydown", onKeyPressModal);
+}
+// ================================================
+
+// =========== Закрытие модального окна ===========
+refs.closeBtn.addEventListener("click", onCloseModal);
+refs.overlay.addEventListener("click", onCloseModal);
+
+function onCloseModal() {
+  refs.lightbox.classList.remove("is-open");
+  refs.lightbox__image.src = "";
+  refs.lightbox__image.alt = "";
+
+  window.removeEventListener("keydown", onKeyPressModal);
+  // IMG_IDX = 0;
+}
+
+function onKeyPressModal(e) {
+  // console.log(e.code);
+  if (e.code === "Escape") {
+    onCloseModal();
+  }
+}
+
+// const onImgClick = (e) => {
+//   refs.atestat.classList.toggle("atestat-clicked");
+//   // console.log(e.target);
+//   // listenBody();
+// };
+
+// refs.atestat.addEventListener("click", onImgClick);
